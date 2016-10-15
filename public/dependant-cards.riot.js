c07b26b449e4a2dcb41dabeed3dface2add6b647
Adrian Unger <dev@staydecent.ca>
2016-10-14 18:57:47 -0700

    Render attachment section via Riot.

---
+++ public/dependant-cards.riot.js
@@ -0,0 +1,28 @@
+<dependant-cards>
+
+  <ul>
+    <li each={ card in items } class="basic-attachment-list-item">
+      { card.name }
+    </li>
+  </ul>
+
+  <script>
+    var t = TrelloPowerUp.iframe()
+
+    document.addEventListener('DOMContentLoaded', function (event) {
+      t.sizeTo('#page')
+    })
+
+    this.items = []
+
+    t.render(function () {
+      console.log('Attachment-section is being rendered.')
+      t.card('attachments').then(function(results) {
+        console.log('Attachments: ', results)
+        this.update({
+          items: results
+        })
+      }.bind(this))
+    })
+  </script>
+</dependant-cards>
